3365f6abfed81d4dc2465bac0dab16baf0561291
Gaurav-Attri <attrigaurav60@gmail.com>
2026-01-22 16:16:51 +0530

    [Gaurav Chaudhary] Add. Added ambulance route system files in scenario-based/ambulance-route-system/

---
+++ data-structures-csharp-programs/scenario-based/ambulance-route-system/IAmbulenceRoute.cs
@@ -0,0 +1,9 @@
+﻿interface IAmbulenceRoute
+{
+    void AddPatient();
+    void ShowAllPatients();
+    void ShowAvailableBuildings();
+    void ShowBuildingsUnderMaintenance();
+    void RedirectPatient();
+    void RemoveBuildingForMaintenance();
+}
---
+++ data-structures-csharp-programs/scenario-based/ambulance-route-system/Patient.cs
@@ -0,0 +1,30 @@
+﻿class Patient
+{
+    private string patientName;
+    private int severity;
+
+    public string GetPatientName()
+    {
+        return patientName;
+    }
+
+    public void SetPatientName(string value)
+    {
+        patientName = value;
+    }
+
+    public int GetSeverity()
+    {
+        return severity;
+    }
+
+    public void SetSeverity(int value)
+    {
+        severity = value;
+    }
+
+    public override string ToString()
+    {
+        return patientName + " | Severity: " + severity;
+    }
+}
